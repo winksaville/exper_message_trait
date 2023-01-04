54ac30bf7a47abb7eb35f2018f9e1275b9a1a1a0
Wink Saville <wink@saville.com>
2023-01-03 17:05:29 -0800

    feat: Add Message trait
    Also, to be fairer on the comparison I added two_channel_using_boxes.
    Bottom line  this does show using trait Message doesn't inheriently slow
    things down so going to try to move forward.

--- README.md
+++ README.md
@@ -3,22 +3,40 @@
 See if I can easily use a message trait to send
 two different Types in one crossbeam_channel.
 
-Initially not using a trait and using two channels.
+Added `trait Message`, this is a trivial trait just
+to see that it was possible.
+
+What I think I need to do is to get the "Type" of the message.
+One option could be to use "std::any::Any" or use "TypeId",
+more exploring to do!
 
 See [src/main.rs](/src/main.rs)
+
 
 ## Run:
 
 ```
-wink@3900x 23-01-03T22:07:18.685Z:~/prgs/rust/myrepos/exper_message_trait (main)
+wink@3900x 23-01-03T23:30:37.607Z:~/prgs/rust/myrepos/exper_message_trait (main)
 $ cargo run --release
     Finished release [optimized] target(s) in 0.00s
      Running `target/release/exper_message_trait`
 time between      instant1 and instant0 50
-time between      instant2 and instant1 30
+time between      instant2 and instant1 20
 time between start_instant and instant2 20
-time_to_send=1433ns time_to_recv=741ns travel_time=2174ns
-(recv_instant - start_instant)=2194ns
+time_to_send=1313ns time_to_recv=681ns travel_time=1994ns
+(recv_instant - start_instant)=2014ns
+
+time between      instant1 and instant0 20
+time between      instant2 and instant1 20
+time between start_instant and instant2 21
+time_to_send=861ns time_to_recv=451ns travel_time=1312ns
+(recv_instant - start_instant)=1332ns
+
+time between      instant1 and instant0 20
+time between      instant2 and instant1 20
+time between start_instant and instant2 20
+time_to_send=552ns time_to_recv=220ns travel_time=772ns
+(recv_instant - start_instant)=792ns
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::time::Instant;
 use crossbeam_channel::unbounded;
 
+pub trait Message {
+    fn get_id(&self) -> u64;
+    fn get_instant(&self) -> Instant;
+    fn get_req_id(&self) -> u64;
+    fn get_req_instant(&self) -> Instant;
+}
+
 #[derive(Clone)]
 pub struct PingReq {
     pub id: u64,
@@ -13,6 +20,24 @@ impl PingReq {
             id,
             instant: Instant::now(),
         }
+    }
+}
+
+impl Message for PingReq {
+    fn get_id(&self) -> u64 {
+        self.id
+    }
+
+    fn get_instant(&self) -> Instant {
+        self.instant
+    }
+
+    fn get_req_id(&self) -> u64 {
+        self.id
+    }
+
+    fn get_req_instant(&self) -> Instant {
+        self.instant
     }
 }
 
@@ -33,10 +58,35 @@ impl PingRsp {
             instant: Instant::now(),
         }
     }
+    fn from(id: u64, req: Box<dyn Message>) -> PingRsp {
+        Self {
+            req_id: req.get_id(),
+            req_instant: req.get_instant(),
+            id,
+            instant: Instant::now(),
+        }
+    }
 }
 
+impl Message for PingRsp {
+    fn get_id(&self) -> u64 {
+        self.id
+    }
 
-fn main() {
+    fn get_instant(&self) -> Instant {
+        self.instant
+    }
+
+    fn get_req_id(&self) -> u64 {
+        self.req_id
+    }
+
+    fn get_req_instant(&self) -> Instant {
+        self.req_instant
+    }
+}
+
+fn two_channels() {
     let (ping_req_tx, ping_req_rx) = unbounded::<PingReq>();
     let (ping_rsp_tx, ping_rsp_rx) = unbounded::<PingRsp>();
 
@@ -70,4 +120,84 @@ fn main() {
     println!("time between start_instant and instant2 {}", start_instant.duration_since(instant2).as_nanos());
     println!("time_to_send={time_to_send}ns time_to_recv={time_to_recv}ns travel_time={travel_time}ns");
     println!("(recv_instant - start_instant)={}ns", main_rtt);
+    println!();
+}
+
+fn two_channels_using_boxes() {
+    let (ping_req_tx, ping_req_rx) = unbounded::<Box<PingReq>>();
+    let (ping_rsp_tx, ping_rsp_rx) = unbounded::<Box<PingRsp>>();
+
+    let instant0 = Instant::now();
+    let instant1 = Instant::now();
+    let instant2 = Instant::now();
+    let start_instant = Instant::now();
+
+    // Create req and send it
+    let req = Box::new(PingReq::new(1));
+    ping_req_tx.send(req).unwrap();
+
+    // Receive response
+    let req_recv = ping_req_rx.recv().unwrap();
+
+    // Send response
+    let rsp = Box::new(PingRsp::new(2, &req_recv));
+    ping_rsp_tx.send(rsp).unwrap();
+
+    // Recv response
+    let rsp_recv = ping_rsp_rx.recv().unwrap();
+    let recv_instant = Instant::now();
+
+    let time_to_send= rsp_recv.instant.duration_since(rsp_recv.req_instant).as_nanos();
+    let time_to_recv= recv_instant.duration_since(rsp_recv.instant).as_nanos();
+    let travel_time = recv_instant.duration_since(rsp_recv.req_instant).as_nanos();
+    let main_rtt = recv_instant.duration_since(start_instant).as_nanos();
+
+    println!("time between      instant1 and instant0 {}", instant1.duration_since(instant0).as_nanos());
+    println!("time between      instant2 and instant1 {}", instant2.duration_since(instant1).as_nanos());
+    println!("time between start_instant and instant2 {}", start_instant.duration_since(instant2).as_nanos());
+    println!("time_to_send={time_to_send}ns time_to_recv={time_to_recv}ns travel_time={travel_time}ns");
+    println!("(recv_instant - start_instant)={}ns", main_rtt);
+    println!();
+}
+
+fn one_channel() {
+    let (tx, rx) = unbounded::<Box<dyn Message>>();
+
+    let instant0 = Instant::now();
+    let instant1 = Instant::now();
+    let instant2 = Instant::now();
+    let start_instant = Instant::now();
+
+    // Create req and send it
+    let req = Box::new(PingReq::new(1));
+    tx.send(req).unwrap();
+
+    // Receive response
+    let req_recv = rx.recv().unwrap();
+
+    // Send response
+    let rsp = Box::new(PingRsp::from(2, req_recv));
+    tx.send(rsp).unwrap();
+
+    // Recv response
+    let rsp_recv = rx.recv().unwrap();
+    let recv_instant = Instant::now();
+
+    let time_to_send= rsp_recv.get_instant().duration_since(rsp_recv.get_req_instant()).as_nanos();
+    let time_to_recv= recv_instant.duration_since(rsp_recv.get_instant()).as_nanos();
+    let travel_time = recv_instant.duration_since(rsp_recv.get_req_instant()).as_nanos();
+    let main_rtt = recv_instant.duration_since(start_instant).as_nanos();
+
+    println!("time between      instant1 and instant0 {}", instant1.duration_since(instant0).as_nanos());
+    println!("time between      instant2 and instant1 {}", instant2.duration_since(instant1).as_nanos());
+    println!("time between start_instant and instant2 {}", start_instant.duration_since(instant2).as_nanos());
+    println!("time_to_send={time_to_send}ns time_to_recv={time_to_recv}ns travel_time={travel_time}ns");
+    println!("(recv_instant - start_instant)={}ns", main_rtt);
+    println!();
+}
+
+fn main() {
+    two_channels();
+    two_channels_using_boxes();
+    one_channel();
 }
